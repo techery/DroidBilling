cdf47eb3bd03a82e5244bcafdfe8133999b8ab85
Sergey Zenchenko <sergeizenchenko@gmail.com>
2013-11-27 22:47:22 +0200

    Remove build folder from git;

--- build/source/buildConfig/debug/com/techery/droid/billings/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.techery.droid.billings;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
